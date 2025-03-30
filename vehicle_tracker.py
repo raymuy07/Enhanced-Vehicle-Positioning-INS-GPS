2313463abe83c07aa651a7958a617b02408a6dc9
DESKTOP-D7MOSLV\Guy <guylei@post.bgu.ac.il>
2025-03-30 17:10:17 +0300

    returned to regular vehicle tracker

--- vehicle_tracker.py
+++ vehicle_tracker.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 from utility_functions import add_gps_error_and_precision, add_communication_distance_error, calculate_distance, trilaterate_gps, calculate_weighted_position
-
 class VehicleTracker:
     def __init__(self, rsu_manager,specific_car_id,error_std_dev,num_of_neighbors,proximity_radius,better_flag):
         self.vehicle_data = {}
@@ -20,9 +19,6 @@ class VehicleTracker:
         self.not_better_values=[]
         self.better_flag=better_flag
 
-        self.weighted_mse = None
-        self.weighted_abs_error = None
-        self.inertial_positions = []
     def update_vehicle_data(self, vehicle_id, geo_position, speed, step):
         if vehicle_id not in self.vehicle_data:
             self.vehicle_data[vehicle_id] = {
@@ -42,27 +38,6 @@ class VehicleTracker:
         perturbed_position, precision_radius = add_gps_error_and_precision(geo_position,self.sat_std)
         self.vehicle_data[vehicle_id]['positions'].append(perturbed_position)
         self.vehicle_data[vehicle_id]['precision_radius'].append(precision_radius)
-
-    def select_positions_for_triangulation(self, positions_distances_radii,step):
-        # Sort positions by precision radius (ascending), so the best precision is first
-        (positions, distances, precision_radius)=positions_distances_radii
-        better=True
-        num_of_parti=self.neighbors_number
-
-        index=np.argsort(precision_radius)[:num_of_parti+1]
-        index = index[:num_of_parti]
-        selected_positions = [positions[i] for i in index]
-        selected_distances = [distances[i] for i in index]
-        selected_accr = [precision_radius[i] for i in index]
-
-        if selected_accr[2]>self.vehicle_data[self.specific_car_id]['precision_radius'][step]:
-            better=False
-        if not(self.better_flag):
-            better=True
-        #print(better)
-        accr = [float(x) for x in selected_accr]
-        print(accr)
-        return (selected_positions,selected_distances,better,selected_accr)
 
     def find_nearby_vehicles_and_check_rsus(self, vehicle_ids, step):
         # Using the real position ONLY in order to calculate the real distances
@@ -102,37 +77,33 @@ class VehicleTracker:
                 self.trilateration_data[step][2].append(precision_radius)
 
 
+    def select_positions_for_triangulation(self, positions_distances_radii,step):
+        # Sort positions by precision radius (ascending), so the best precision is first
+        (positions, distances, precision_radius)=positions_distances_radii
+        better=True
+        num_of_parti=self.neighbors_number
 
-    def calculate_mse_satellite_positions(self):
-        squared_errors = []
-        avg = []
-        for step in range(len(self.vehicle_data[self.specific_car_id]['positions'])):
-            perturbed_position = self.vehicle_data[self.specific_car_id]['positions'][step]
-            if step < len(self.vehicle_data[self.specific_car_id]['real_positions']):
-                actual_position = self.vehicle_data[self.specific_car_id]['real_positions'][step]
-                error = calculate_distance(perturbed_position, actual_position)
-                squared_errors.append(error ** 2)
-                avg.append(error)
-        if squared_errors:
-            mse = np.mean(squared_errors)
-            error = np.mean(avg)
-            self.sat_abs_error = error
-            self.sat_mse = mse
-            print(f"MSE for Satellite Positions: {mse:.2f} meters squared")
-            print(f"Abs Error for Satellite Positions: {error:.2f} meters ")
+        index=np.argsort(precision_radius)[:num_of_parti+1]
+        index = index[:num_of_parti]
+        selected_positions = [positions[i] for i in index]
+        selected_distances = [distances[i] for i in index]
+        selected_accr = [precision_radius[i] for i in index]
 
-        else:
-            print("No data available to calculate MSE for Satellite Positions.")
-        print()
+        if selected_accr[2]>self.vehicle_data[self.specific_car_id]['precision_radius'][step]:
+            better=False
+        if not(self.better_flag):
+            better=True
+        #print(better)
+        accr = [float(x) for x in selected_accr]
+        print(accr)
+        return (selected_positions,selected_distances,better,selected_accr)
+
 
     def print_estimated_positions_and_errors(self,alpha,want_print):
         # Initialize variables for error accumulation
         squared_errors = []
         absolute_errors = []
-        weighted_squared_errors = []
-        weighted_absolute_errors = []
-        navig_absolute_errors=[]
-        navig_squared_errors=[]
+
         FA=0
         MD=0
         TP=0
@@ -179,38 +150,20 @@ class VehicleTracker:
 
                     self.vehicle_data[self.specific_car_id]['estimated_position'].append(estimated_position)
 
-                    # Calculate the weighted position
-                    weight_inertial = 0.5
-                    weight_trilateration = 0.5
-                    inertial_position = self.inertial_positions[actual_position_index-1]  # Use the inertial position
-                    weighted_position = calculate_weighted_position(inertial_position, estimated_position,weight_inertial, weight_trilateration)
-
-                    weighted_error = calculate_distance(weighted_position, actual_position)
-                    iner_error=calculate_distance(inertial_position,actual_position)
-
                     # Accumulate errors for MSE and absolute error calculations
                     squared_errors.append(triangulation_error ** 2)
                     absolute_errors.append(abs(triangulation_error))
-                    weighted_squared_errors.append(weighted_error ** 2)
-                    weighted_absolute_errors.append(abs(weighted_error))
-                    navig_squared_errors.append(iner_error**2)
-                    navig_absolute_errors.append(abs(iner_error))
-
                     if want_print:
                         print(
-                            f"  Step {step}: Estimated Position (trilateration) - Lat: {estimated_position[0]}, Lon: {estimated_position[1]} | "
-                            f"Actual Position - Lat: {actual_position[0]}, Lon: {actual_position[1]} | "
-                            f"Weighted Position (inertial + trilateration) - Lat: {weighted_position[0]}, Lon: {weighted_position[1]} | "
+                            f"  Step {step}: Estimated Position - Lat: {estimated_position[0]}, Lon: {estimated_position[1]} | "
                             f"Triangulation Error: {triangulation_error:.2f} meters | "
                             f"Satellite Position Error: {satellite_position_error:.2f} meters | "
-                            f"Weighted Position Error: {weighted_error:.2f} meters | "
                             f"Number of participants: {len(positions)}")
                 else:
                     print(f"  Step {step}: Data unavailable for real position.")
             else:
                 print(f"  Step {step}: Not enough data for trilateration.")
         #plot_errors(absolute_errors,squared_errors)
-        '''
         uses_of_better=100*not_beter_count / len(squared_errors)
         print(f"\n{uses_of_better:.2f}% of the position are the Satellite,{not_beter_count} Times")
         print(f"FA: {FA} Times, {FA*100/len(squared_errors):.2f}%")
@@ -218,38 +171,72 @@ class VehicleTracker:
         print(f"FN: {FN} Times, {FN*100/len(squared_errors):.2f}%")
         print(f"TP: {TP} Times, {TP*100/len(squared_errors):.2f}%")
 
-'''
+
         # Calculate and print the MSE and the average (absolute) error
         if squared_errors:
             mse = sum(squared_errors) / len(squared_errors)
             avg_absolute_error = sum(absolute_errors) / len(absolute_errors)
-            print()
-            print("Trilateration Errors:")
-            print(f"MSE for Estimated Positions: {mse:.2f} meters squared")
+            print(f"\nMSE for Estimated Positions: {mse:.2f} meters squared")
             print(f"Average Absolute Error for Estimated Positions: {avg_absolute_error:.2f} meters")
+        self.errors_results.append((avg_absolute_error, mse,uses_of_better ))
 
+    def calculate_mse_satellite_positions(self):
+        squared_errors = []
+        avg=[]
+        for step in range(len(self.vehicle_data[self.specific_car_id]['positions'])):
+            perturbed_position = self.vehicle_data[self.specific_car_id]['positions'][step]
+            if step < len(self.vehicle_data[self.specific_car_id]['real_positions']):
+                actual_position = self.vehicle_data[self.specific_car_id]['real_positions'][step]
+                error = calculate_distance(perturbed_position, actual_position)
+                squared_errors.append(error ** 2)
+                avg.append(error)
+        if squared_errors:
+            mse = np.mean(squared_errors)
+            error=np.mean(avg)
+            self.sat_abs_error=error
+            self.sat_mse=mse
+            print(f"MSE for Satellite Positions: {mse:.2f} meters squared")
+            print(f"Abs Error for Satellite Positions: {error:.2f} meters ")
+
+        else:
+            print("No data available to calculate MSE for Satellite Positions.")
         print()
 
-        if iner_error:
-            iner_mse = sum(navig_squared_errors) / len(navig_squared_errors)
-            iner_avg_absolute_error = sum(navig_absolute_errors) / len(navig_absolute_errors)
-            print("inertial Errors:")
-            print(f"MSE for Estimated Positions: {iner_mse:.2f} meters squared")
-            print(f"Average Absolute Error for Estimated Positions: {iner_avg_absolute_error:.2f} meters")
+    def print_error_results_of_sweep(self,start,step):
+        for i, (avg_absolute_error, mse, uses_of_better) in enumerate(self.errors_results):
+            print(f"For alpha = {start+i*step}:")
+            print(f"  Average Absolute Error: {avg_absolute_error:.2f}.     improve of: {(self.sat_abs_error-avg_absolute_error)*100/self.sat_abs_error:.2f}%")
+            print(f"  Mean Squared Error: {mse:.2f}.           improve of: {(self.sat_mse-mse)*100/self.sat_mse:.2f}%")
+            print(f"  Uses of Better: {uses_of_better:.2f}%")
+            print()
 
-        if weighted_squared_errors:
-            weighted_mse = sum(weighted_squared_errors) / len(weighted_squared_errors)
-            weighted_abs_error = sum(weighted_absolute_errors) / len(weighted_absolute_errors)
-            self.weighted_mse = weighted_mse
-            self.weighted_abs_error = weighted_abs_error
-            print("Weighted Errors:")
-            print(f"\nMSE for Weighted Positions: {weighted_mse:.2f} meters squared")
-            print(f"Average Absolute Error for Weighted Positions: {weighted_abs_error:.2f} meters")
+    def print_error_results(self):
 
-
+        for i, (avg_absolute_error, mse, uses_of_better) in enumerate(self.errors_results):
+            print(f"  Average Absolute Error: {avg_absolute_error:.2f}.     improve of: {(self.sat_abs_error-avg_absolute_error)*100/self.sat_abs_error:.2f}%")
+            print(f"  Mean Squared Error: {mse:.2f}.        improve of: {(self.sat_mse-mse)*100/self.sat_mse:.2f}%")
+            print(f"  Uses of Better: {uses_of_better:.2f}%")
+            print()
         print()
-        print(f"weighted helped Trilateration improve Absolute Error by: {(mse-weighted_mse)*100/mse:.2f}%")
-        print(f"weighted helped Trilateration improve Absolute Error by: {(avg_absolute_error-weighted_abs_error)*100/avg_absolute_error:.2f}%")
+        print("MD- We think our result is better but it is not")
+        print("TD- We think our result is better and rightly so")
+        print("FA- We think our result is not better but it is")
+        print("TA- We think our result is no better and it really isn't")
+        print()
+        total=len(self.MD_errors)+len(self.better_values)+len(self.FA_errors)+len(self.not_better_values)
+        print(f"E[MD]- when we wrong we by:             {np.mean(self.MD_errors):.2f} meter     {len(self.MD_errors)*100/total:.2f}% of the time")
+        print(f"E[TD]- when we right we by:             {np.mean(self.better_values):.2f} meter     {len(self.better_values)*100/total:.2f}% of the time")
+        print(f"E[FA]- when better=false we wrong by:   {np.mean(self.FA_errors):.2f} meter     {len(self.FA_errors)*100/total:.2f}% of the time")
+        print(f"E[TA]- using better, saves:             {np.mean(self.not_better_values):.2f} meter     {len(self.not_better_values)*100/total:.2f}% of the time")
 
+    def sweep_alpha(self,vehicle_tracker,start,end,step,want_print):
+        for i in np.arange(start, end + step, step):
+            vehicle_tracker.print_estimated_positions_and_errors(1, want_print)
+            print(i)
 
+    def sweep_neighbors(self,vehicle_tracker,start,end,step,want_print):
+        for i in np.arange(start, end + step, step):
+            self.neighbors_number=i
+            vehicle_tracker.print_estimated_positions_and_errors(1, want_print)
+            print(i)
 
