03d44b0c7f7443da5f0ef104b7a2cec3816387cc
DESKTOP-D7MOSLV\Guy <guylei@post.bgu.ac.il>
2025-05-19 09:58:40 +0300

    poc of changing input simulation method

--- main.py
+++ main.py
@@ -53,6 +53,56 @@ if __name__ == "__main__":
 
     print(f"Running simulation: {simulation_type}")
 
+    # # Step 1: Choose simulation scenario
+    # sim = int(input("Choose simulation scenario:\n1- Route 90\n2- Haifa\n3- Manhattan\n"))
+    #
+    # if sim == 1:
+    #     simulation_path = "Sumo/route_90_high_traffic/osm.sumocfg"
+    #     simulation_type = 'Route_90'
+    #     gps_error_std = 5  # Low error – open highway
+    # elif sim == 2:
+    #     simulation_path = "Sumo/Haifa/osm.sumocfg"
+    #     simulation_type = 'Haifa'
+    #     gps_error_std = 8  # Medium error – mid-sized city
+    # elif sim == 3:
+    #     simulation_path = "Sumo/manhattan_high_traffic/osm.sumocfg"
+    #     simulation_type = 'Manhattan'
+    #     gps_error_std = 10  # High error – dense urban area
+    # else:
+    #     raise ValueError(f"Invalid simulation type: {sim}")
+    #
+    # print(f"Selected simulation: {simulation_type}")
+    #
+    # # Step 2: Choose traffic density
+    # density = int(input("Choose traffic density:\n1- Low\n2- Medium\n3- High\n"))
+    #
+    # if density == 1:
+    #     insertion_density = 1000
+    # elif density == 2:
+    #     insertion_density = 3000
+    # elif density == 3:
+    #     insertion_density = 5000
+    # else:
+    #     raise ValueError(f"Invalid traffic density: {density}")
+    #
+    # print(f"Selected traffic density: {insertion_density} vehicles/hour/km")
+    #
+    # # Step 3: Set simulation parameters
+    # simulation_params = {
+    #     'gps_refresh_rate': 10,
+    #     'dsrc_refresh_rate': 5,
+    #     'ins_refresh_rate': 1,
+    #     'number_of_steps': 600,
+    #     'gps_error_model_std': gps_error_std,  # Set based on scenario
+    #     'communication_error_model_std': 2,
+    #     'systematic_bias': 0.3,
+    #     'proximity_radius': 300,
+    #     'rsu_flag': True,
+    #     'insertion_density': insertion_density  # New addition
+    # }
+
+
+
     simulation_manager = SimulationManager(simulation_params, simulation_type, gps_error_model, comm_error_model)
 
     ##TODO change the specific_car_id method to be more dynamic
